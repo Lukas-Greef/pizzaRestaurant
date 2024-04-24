32012f0dea49e9a9e471997195a4678098daab8c
Lukas-Greef <1209628@student.roc-nijmegen.nl>
2024-04-24 11:13:12 +0200

    x

--- php/zoek-balk.php
+++ php/zoek-balk.php
@@ -1,22 +1,28 @@
 <?php
 
+$conn = new PDO('mysql:dbname=Pizza toko;host=mysql_db','root' ,'rootpassword');
+require_once 'connect.php';
 $sql = "INSERT INTO Menu(titel, prijs, beschrijving, img) VALUES (:titel, :prijs, :beschrijving, :img)";
-
-if (isset($_GET['search'])) {
-    $searchTerm = $_GET['search'];
-    $searchQuery = "SELECT * FROM Menu WHERE titel LIKE :zoekeninput";
-    $stmt_search = $pdo->prepare($searchQuery);
-    $searchParam = "%" . $searchTerm . "%";
-    $stmt_search->bindParam(":zoekinput", $searchParam);
-    $stmt_search->execute();
-} else {
-    $stmt_all = $PDO->query("SELECT * FROM Menu");
-}
 ?>
+<form action="" method="get">
 <div class="zoekbalk-achtergrond center">
     <img class="logo-zoekbalk margin-left" src="afbeeldingen/zoekbalk-logo.jpeg" alt="een plaatje voor naast de zoekbalk">
     <input type="text" placeholder="Zoeken" name="zoekinput" class="zoeken margin-left font">
+    <button type="submit">Zoeken</button>
 </div>
+</form>
+<?php
+if (isset($_GET['zoekinput'])) {
+    $searchTerm = $_GET['zoekinput'];
+    $searchQuery = "SELECT * FROM Menu WHERE titel LIKE :zoekeninput";
+    $stmt_search = $pdo->prepare($searchQuery);
+    $searchParam = "%" . $searchTerm . "%";
+    $stmt_search->bindParam(":zoekeninput", $searchParam);
+    $stmt_search->execute();
+} else {
+    $stmt_all = $pdo->query("SELECT * FROM Menu");
+}
+?>
 
 
 <?php
